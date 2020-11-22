85cba02a111e8e8cc825f39411a43b98cf3c395f
Abigail <41799687+abigailjxn@users.noreply.github.com>
2020-11-21 21:33:10 -0600

    add: working accessible accordion; added focus to video grid elements

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   // Header Menu
   const hamburgerElem = $(".hamburger");
 
+  // A11Y Note: <button> tag will fire a 'click' event on Enter or Spacebar, so adding a keyup event isn't necessary in those cases.
   hamburgerElem.keyup((e) => {
     if (e.keyCode === 13 || e.keyCode === 32) {
       hamburgerElem.click();
@@ -12,6 +13,28 @@ $(document).ready(function () {
     e.preventDefault(); // stops link from making page jump to the top
     e.stopPropagation(); // when you click the button, it stops the page from seeing it as clicking the body too
     $("html").toggleClass("navOpen");
+  });
+
+  // Accordion
+  const accordionButtons = document.querySelectorAll(".accordion__button");
+  console.log(accordionButtons);
+  accordionButtons.forEach((button) => {
+    const btnID = button.getAttribute("id").split("-").pop();
+    const accordionSection = document.getElementById(
+      `accordion-section-${btnID}`
+    );
+    button.addEventListener('click', () => {
+      button.classList.toggle('expanded');
+      accordionSection.classList.toggle('open');
+
+      if(button.classList.contains('open')){
+        accordionSection.setAttribute('aria-expanded', true);
+        accordionSection.setAttribute('aria-hidden', false);
+      } else{
+        accordionSection.setAttribute('aria-expanded', false)
+        accordionSection.setAttribute('aria-hidden', true)
+      }
+    })
   });
 
   //Mobile Hover Alternative
