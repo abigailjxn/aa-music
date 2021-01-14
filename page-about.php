3a6b5e948a1fda237d295c61518a58bbf5ad7577
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2021-01-14 15:01:59 -0600

    acf(about): updated about page with acf; started responsive

--- page-about.php
+++ page-about.php
@@ -1,6 +1,10 @@
 <?php
 
 get_header();
+$training = get_field('training');
+$experience = get_field('experience');
+$inspiration = get_field('inspiration');
+$cta = get_field('about_cta');
 
 
 ?>
@@ -25,12 +29,9 @@ get_header();
         <div class="row">
             <div class="flex-4">
                 <h2 class="sansHeader-lg normal">
-                    Classically trained,<br>
-                    Story inspired
+                    <?php echo $training['header']; ?>
                 </h2>
-                <p class="sansSerif-sm normal">Amanda is native to the cornfields of Illinois where she began studying piano at a young age. She’s always had an affinity for story telling.</p>
-                <p class="sansSerif-normal bold">She credits Pirates of the Caribbean as the movie that first solidified her love of film scores. She became interested in how music can influence a narrative and decided composition was her most natural form of artistic expression.</p>
-                <p class="sansSerif-sm normal">After completing a Bachelor of Arts degree in music and vocal performance from North Central College, she joined Comma Music in Chicago as a staff composer for two years until transitioning to freelance.</p>
+                <?php echo $training['content']; ?>
             </div>
 
             <!-- birds illustration  -->
@@ -44,13 +45,11 @@ get_header();
                 <div class="flex-7">
                     
                 </div>
-
                 <div class="flex-4 push-1">
                     <h2 class="sansHeader-lg normal">
-                        Maybe you've heard
+                        <?php echo $experience['header']; ?>
                     </h2>
-                    <p class="sansSerif-sm normal">She has worked with major advertising agencies in Chicago, L.A., Milwaukee, New York, Portland, and San Francisco.<br>Her original music can be heard in national campaign spots with Boeing, Capital One, Credit Karma, Feeding America, Hallmark, Jim Beam, McDonald's, Raid, Sentry Insurance, and Society Nine.<br>Additionally, she has scored three short films: Northfound (2016), The Lion and the Gecko (2018), and Revolt of Color (2017).
-                    </p>
+                    <?php echo $experience['content']; ?>
                 </div>
         </div>
     </div>
@@ -59,7 +58,7 @@ get_header();
 <section class="collab">
     <div class="container">
         <div class="row">
-            <h2 class="sansHeader-lg normal">Music Heard in Collaboration<br>With These Companies</h2>
+            <h2 class="sansHeader-lg normal"><?php echo $experience['logo_header']; ?></h2>
             <ul class="logo-list">
                 <li class="logo">
                     <img src="" alt="">
@@ -99,10 +98,9 @@ get_header();
         <div class="row">
             <div class="flex-4">
                 <h2 class="sansHeader-lg normal">
-                    Sounds of<br>Inspiration
+                    <?php echo $inspiration['header']; ?>
                 </h2>
-                <p class="sansSerif-sm normal">Amanda’s writing style is an amalgamation of her love for anime, fantasy films, and video games. Drawing from composers such as Howard Shore (Lord of the Rings), Joe Hisaishi (Spirited Away), and Thomas Newman (Shawshank Redemption), her music embodies piano driven melodies, ethereal themes, and classical elements. As an artist, creating content that will move and inspire has always been her top priority.
-                </p>
+                <?php echo $inspiration['content']; ?>
             </div>
         </div>
     </div>
@@ -114,7 +112,7 @@ get_header();
         <div class="row">
             <div class="text">
                 <h2 class="sansHeader-lg normal">
-                    Let's Make Some Music
+                    <?php echo $cta['header']; ?>
                 </h2>
                 <a href="" class="button">Get in touch</a>
             </div>
